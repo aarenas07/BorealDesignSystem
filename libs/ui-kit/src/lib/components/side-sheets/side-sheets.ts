3ef9b03b019a8ba37716aa386fae9221f38dec62
Edgar Munoz Q <edgmuo1085@gmail.com>
2025-12-05 08:40:52 -0500

    se implementa side-sheets de angular material

--- libs/ui-kit/src/lib/components/side-sheets/side-sheets.ts
+++ libs/ui-kit/src/lib/components/side-sheets/side-sheets.ts
@@ -1,9 +1,81 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FocusTrapFactory, FocusTrap } from '@angular/cdk/a11y';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ElementRef,
+  HostListener,
+  TemplateRef,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
 @Component({
   selector: 'lib-side-sheets',
-  imports: [],
+  standalone: true,
   templateUrl: './side-sheets.html',
-  styleUrl: './side-sheets.scss',
+  styleUrls: ['./side-sheets.scss'],
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideSheetsComponent {}
+export class SideSheetsComponent {
+  @Input() open = false;
+  @Input() position: 'start' | 'end' = 'end';
+  @Input() size: 'sm' | 'md' | 'lg' | 'full' = 'md';
+  @Input() modal = true;
+  @Input() closeOnBackdropClick = true;
+  @Input() header?: string | TemplateRef<any>;
+  @Input() actions?: TemplateRef<any>;
+
+  @Output() openChange = new EventEmitter<boolean>();
+  @Output() closed = new EventEmitter<void>();
+
+  private focusTrap?: FocusTrap;
+  private previouslyFocused?: Element | null;
+
+  constructor(
+    private el: ElementRef,
+    private focusTrapFactory: FocusTrapFactory
+  ) {}
+
+  ngOnChanges() {
+    if (this.open) {
+      this.activateTrap();
+    } else {
+      this.destroyTrap();
+    }
+  }
+
+  activateTrap() {
+    this.previouslyFocused = document.activeElement;
+    this.focusTrap = this.focusTrapFactory.create(
+      this.el.nativeElement.querySelector('.side-sheet')
+    );
+    this.focusTrap.focusInitialElement();
+  }
+
+  destroyTrap() {
+    this.focusTrap?.destroy();
+    if (this.previouslyFocused instanceof HTMLElement) {
+      (this.previouslyFocused as HTMLElement).focus();
+    }
+  }
+
+  close() {
+    this.open = false;
+    this.openChange.emit(false);
+    this.closed.emit();
+  }
+
+  onBackdropClick() {
+    if (this.closeOnBackdropClick) this.close();
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  onEscape(event: KeyboardEvent) {
+    if (this.open) {
+      this.close();
+    }
+  }
+}

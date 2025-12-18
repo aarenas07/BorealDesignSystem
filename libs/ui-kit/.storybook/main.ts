38ab8eab8ed20399440fd414968fd248c7d91be7
Edgar Munoz Q <edgmuo1085@gmail.com>
2025-12-18 15:17:12 -0500

    se deja solo los controles y acctiones

--- libs/ui-kit/.storybook/main.ts
+++ libs/ui-kit/.storybook/main.ts
@@ -5,7 +5,7 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/angular',
     options: {},

00e67f463d2fea8722e74183bae95c91d31bb48b
Edgar Munoz Q <edgmuo1085@gmail.com>
2025-12-22 12:31:05 -0500

    se ajusta autocomplete para las diferentes funcionalidades

--- libs/ui-kit/src/lib/components/autocomplete/autocomplete.ts
+++ libs/ui-kit/src/lib/components/autocomplete/autocomplete.ts
@@ -1,5 +1,5 @@
-import { Component, computed, ElementRef, input, OnInit, signal, TemplateRef, ViewChild } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, computed, effect, ElementRef, input, model, OnInit, signal, TemplateRef, ViewChild } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -15,6 +15,11 @@ export type AutocompleteOption = {
 @Component({
   selector: 'bds-autocomplete',
   imports: [FormsModule, MatFormFieldModule, MatInputModule, MatAutocompleteModule, ReactiveFormsModule],
+  host: {
+    '[class.full-width]': 'fullWidth()',
+    '[class.has-error]': 'formControl.invalid && formControl.touched',
+    '[class.is-disabled]': 'disabled()',
+  },
   templateUrl: './autocomplete.html',
   styleUrl: './autocomplete.scss',
 })
@@ -23,19 +28,20 @@ export class AutocompleteComponent implements OnInit {
   appearance = input<AutocompleteAppearance>('outline');
   fullWidth = input<boolean>(false);
 
-  disabledInput = input<boolean>(false);
+  autoActiveFirstOption = input<boolean>(false);
+  autocompleteDisabled = input<boolean>(false);
+  disabled = input<boolean>(false);
   required = input<boolean>(false);
-  readonlyInput = input<boolean>(false);
-  activeFirstOption = input<boolean>(false);
 
   placeholder = input<string>('');
   hint = input<string>('');
   customError = input<string>('');
 
-  formControl = new FormControl('');
+  formControl = new FormControl<string | object | null>('');
   options = input<AutocompleteOption[]>([]);
-  //filteredOptions: Observable<any[]> = of([]);
   filteredOptions = signal<AutocompleteOption[]>([]);
+
+  value = model<any | null>(null);
 
   errorMessage = computed(() => {
     // Si hay un error personalizado, mostrarlo
@@ -60,53 +66,130 @@ export class AutocompleteComponent implements OnInit {
 
   @ViewChild('input') input!: ElementRef<HTMLInputElement>;
 
+  constructor() {
+    effect(() => {
+      const currentValue = this.value();
+      if (this.formControl.value !== currentValue) {
+        const filterValue = this.filterOptions(currentValue);
+        this.formControl.setValue(filterValue, { emitEvent: false });
+      }
+    });
+
+    effect(() => {
+      if (this.disabled()) {
+        this.formControl.disable();
+      } else {
+        this.formControl.enable();
+      }
+    });
+
+    // Actualizar validadores cuando cambien el input
+    effect(() => {
+      this.updateValidators();
+    });
+
+    // Sincronizar el model con el valor del FormControl
+    this.formControl.valueChanges.subscribe(newValue => {
+      this.value.set(newValue || null);
+    });
+
+    effect(() => {
+      if (this.customError()) {
+        this.formControl.setErrors({ customError: true });
+      } else {
+        this.formControl.setErrors(null);
+      }
+    });
+  }
+
   ngOnInit() {
     this.filteredOptions.set(this.options());
   }
 
   filter(): void {
     const filterValue = this.input.nativeElement.value.toLowerCase();
-    console.log('filterValue: ', filterValue);
 
     if (!filterValue) {
-      console.log('No filterValue');
       this.filteredOptions.set(this.options());
       return;
     }
 
     if (!this.options().length) {
-      console.log('No options');
       this.filteredOptions.set([]);
       return;
     }
 
-    if (this.options()[0].group) {
-      console.log('filterValue is group');
+    if (this.isGrouped()) {
+      let copyOptions = structuredClone(this.options());
 
-      let search = this.options();
-      search = search.filter(o => {
-        console.log(o.group);
+      copyOptions = copyOptions.filter(o => {
         if (o.group) {
           o.group = o.group.filter(g => g.label.toString().toLowerCase().includes(filterValue));
         }
+        if (o.group?.length === 0) {
+          return null;
+        }
+
+        return o;
       });
-      console.log('search: ', search);
-      this.filteredOptions.set(search);
+
+      this.filteredOptions.set(copyOptions);
       return;
     }
 
     if (typeof filterValue === 'string') {
-      console.log('filterValue is string');
       const search = this.options().filter(o => o.label.toString().toLowerCase().includes(filterValue));
       this.filteredOptions.set(search);
       return;
     }
 
-    console.log('filterValue is not string');
     this.filteredOptions.set([]);
+  }
+
+  private filterOptions(currentValue: string | object): string | object {
+    if (!currentValue) {
+      return '';
+    }
+
+    if (!this.options().length) {
+      return currentValue;
+    }
+
+    if (this.isGrouped()) {
+      let copyOptions = structuredClone(this.options());
+      let foundOption = null;
+
+      copyOptions.forEach(o => {
+        if (o.group) {
+          foundOption = o.group.find(g => g.label.toString().toLowerCase() === currentValue.toString().toLowerCase());
+        }
+      });
+
+      return foundOption || '';
+    }
+
+    const foundOption = this.options().find(o => o.label.toString().toLowerCase() === currentValue.toString().toLowerCase());
+    return foundOption || '';
   }
 
   displayFn(item: AutocompleteOption): string {
     return item && item.label ? item.label.toString() : '';
+  }
+
+  isGrouped(): boolean {
+    return this.options().some(o => o.group);
+  }
+
+  private updateValidators(): void {
+    const validators: ValidatorFn[] = [];
+
+    // Required validator
+    if (this.required()) {
+      validators.push(Validators.required);
+    }
+
+    // Aplicar validadores
+    this.formControl.setValidators(validators);
+    this.formControl.updateValueAndValidity();
   }
 }

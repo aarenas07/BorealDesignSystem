5bbbf9c54ad64bf7158f4decc53b2570aa34c89c
Edgar Munoz Q <edgmuo1085@gmail.com>
2025-12-30 10:12:53 -0500

    se crea componente

---
+++ libs/ui-kit/src/lib/components/select/select.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-select',
+  imports: [],
+  templateUrl: './select.html',
+  styleUrl: './select.scss',
+})
+export class SelectComponent {}

d40ebbbc5334734562ed615cb78eba77e225bbab
Edgar Munoz Q <edgmuo1085@gmail.com>
2026-01-05 10:18:56 -0500

    se crea storybook del checkbox

---
+++ libs/ui-kit/src/lib/components/checkbox/checkbox.stories.ts
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { CheckboxComponent } from './checkbox';
+import { moduleMetadata } from '@storybook/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const meta: Meta<CheckboxComponent> = {
+  title: 'Atomos/Checkbox',
+  component: CheckboxComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [CheckboxComponent, ReactiveFormsModule, BrowserAnimationsModule],
+    }),
+  ],
+  args: {
+    label: 'Checkbox',
+    disabled: false,
+    checked: false,
+    indeterminate: false,
+    positionLabel: 'after',
+  },
+  argTypes: {
+    label: { control: 'text' },
+    disabled: { control: 'boolean' },
+    checked: { control: 'boolean' },
+    indeterminate: { control: 'boolean' },
+    positionLabel: { control: 'radio', options: ['before', 'after'] },
+  },
+};
+
+export default meta;
+type Story = StoryObj<CheckboxComponent>;
+
+export const Basic: Story = {
+  args: {
+    label: 'Label checkbox',
+    disabled: false,
+    checked: false,
+    indeterminate: false,
+    positionLabel: 'after',
+  },
+};

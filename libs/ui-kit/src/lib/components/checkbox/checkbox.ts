0a0d779c89eb6bb972d5ab8df70161e1d39b1caa
Edgar Munoz Q <edgmuo1085@gmail.com>
2026-01-05 07:55:34 -0500

    se agrega componente nuevo

---
+++ libs/ui-kit/src/lib/components/checkbox/checkbox.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-checkbox',
+  imports: [],
+  templateUrl: './checkbox.html',
+  styleUrl: './checkbox.scss'
+})
+export class Checkbox {}

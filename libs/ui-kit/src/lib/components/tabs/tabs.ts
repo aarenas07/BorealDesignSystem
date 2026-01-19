8bbc1a66794ef8473b5f0b481fb0a938aa9d4c0e
Edgar Munoz Q <edgmuo1085@gmail.com>
2026-01-19 11:19:52 -0500

    se agrega componente tabs

---
+++ libs/ui-kit/src/lib/components/tabs/tabs.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-tabs',
+  imports: [],
+  templateUrl: './tabs.html',
+  styleUrl: './tabs.scss'
+})
+export class Tabs {}

49218b14e296262f7a914a8b302910648b117e42
Edgar Munoz Q <edgmuo1085@gmail.com>
2026-01-19 16:50:01 -0500

    se agrega nuevo componente

---
+++ libs/ui-kit/src/lib/components/snackbar/snackbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bds-snackbar',
+  imports: [],
+  templateUrl: './snackbar.html',
+  styleUrl: './snackbar.scss',
+})
+export class SnackbarComponent {}

d18c16185b04114c52c27e21b04c969cfeaab17b
Edgar Munoz Q <edgmuo1085@gmail.com>
2026-01-23 16:33:03 -0500

    se pasa cambios de develop para sincronizar ramas

--- libs/ui-kit/src/lib/components/expasion-panel/expansion-panel.ts
+++ libs/ui-kit/src/lib/components/expasion-panel/expansion-panel.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,4 +9,17 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './expansion-panel.html',
   styleUrl: './expansion-panel.scss',
 })
-export class ExpansionPanelComponent {}
+export class ExpansionPanelComponent {
+  @Input() headerColor = '';
+  @Input() contentColor = '';
+
+  @HostBinding('style.--bds-expansion-panel-header-bg')
+  get headerBgVar(): string | null {
+    return this.headerColor || null;
+  }
+
+  @HostBinding('style.--bds-expansion-panel-content-bg')
+  get contentBgVar(): string | null {
+    return this.contentColor || null;
+  }
+}

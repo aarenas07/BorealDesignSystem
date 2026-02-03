db2dcac69ad23fa7b29089c3ddba4b8ca591d27a
Edgar Munoz Q <edgmuo1085@gmail.com>
2026-02-03 15:35:49 -0500

    se ajusta nombre componente

---
+++ libs/ui-kit/src/lib/components/searchbar/searchbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-searchbar',
+  imports: [],
+  templateUrl: './searchbar.html',
+  styleUrl: './searchbar.scss',
+})
+export class Searchbar {}
